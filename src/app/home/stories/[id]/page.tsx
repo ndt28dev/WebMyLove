c87d74224d418f96cfae1e12f8bbaa4dde799ce3
Nguyễn Duy Thuần <ndt28dev@gmail.com>
2025-09-14 01:07:52 +0700

    fix: PageId in stories

--- src/app/home/stories/[id]/page.tsx
+++ src/app/home/stories/[id]/page.tsx
@@ -4,16 +4,25 @@ import { loveStories } from "@/modules/data/stories/data";
 import type { IStory } from "@/modules/interface/IStory";
 import StoryDetailClient from "@/container/home/stories/id/StoryDetailClient";
 
-export default function Page({ params }: { params: { id: string } }) {
-  const { id } = params;
+export default async function page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
   const story = loveStories.find((s) => String(s.id) === id) as
     | IStory
     | undefined;
-  if (!story) return notFound();
+  if (!story) notFound();
 
   return (
     <Container size="lg" pt="md">
       <StoryDetailClient story={story} />
     </Container>
   );
+}
+
+export async function generateStaticParams() {
+  return loveStories.map((s) => ({ id: String(s.id) }));
 }
